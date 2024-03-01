d180d60cb326b8274cccb855b37e10928eb9d1ca
Sidharth Tripathi <sidharhtripathi807@gmail.com>
2024-03-01 19:31:05 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ const server = new SMTPServer({
     onData(stream,session,callback){
         simpleParser(stream,(err,mailParsed)=>{
             if(err) console.log(err);
-            console.log(mailParsed);
+            console.log(mailParsed.from,mailParsed.to,mailParsed.text);
             stream.on("end",callback)
         })
     },
     disabledCommands : ["AUTH"]
 })
 
-server.listen(25)
+server.listen(25,()=>{
+    console.log("SMTP server listening for incoming mails")
+})
